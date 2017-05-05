376cd1645c8c16ace98c1bb436abc152d3cd3897
lekkiduchem <m.liebiediew@gmail.com>
2017-05-05 17:05:47 +0200

    you now know matrix multiplication

--- Octave/testing/matrix_multiplication.m
+++ Octave/testing/matrix_multiplication.m
@@ -7,37 +7,28 @@ y = [ 2, 1 ;
       2, 2 ;
       2, 3 ]
 
-printf(" x (%d,%d)  * y (%d,%d)  = (%d,%d) | ", size(x ), size(y ), size(x  * y )), ifelse(d, x  * y , "")
-printf(" y (%d,%d)  * x (%d,%d)  = (%d,%d) | ", size(y ), size(x ), size(y  * x )), ifelse(d, y  * x , "")
-printf(" x'(%d,%d)  * y'(%d,%d)  = (%d,%d) | ", size(x'), size(y'), size(x' * y')), ifelse(d, x' * y', "")
-printf(" y'(%d,%d)  * x'(%d,%d)  = (%d,%d) | ", size(y'), size(x'), size(y' * x')), ifelse(d, y' * x', "")
-  % x (2,3)  * y (3,2) = (2,2)
-  % y (3,2)  * x (2,3) = (3,3)
-  % x'(3,2)  * y'(2,3) = (3,3)
-  % y'(2,3)  * x'(3,2) = (2,2)
-
-printf(" x'(%d,%d) .* y (%d,%d)  = (%d,%d) | ", size(x'), size(y ), size(x' .* y )), ifelse(d, x' .* y , "")
-printf(" x (%d,%d) .* y'(%d,%d)  = (%d,%d) | ", size(x ), size(y'), size(x  .* y')), ifelse(d, x  .* y', "")
-printf(" y'(%d,%d) .* x (%d,%d)  = (%d,%d) | ", size(y'), size(x ), size(y' .* x )), ifelse(d, y' .* x , "")
-printf(" y (%d,%d) .* x'(%d,%d)  = (%d,%d) | ", size(y ), size(x'), size(y  .* x')), ifelse(d, y  .* x', "")
-  % x'(3,2) .* y (3,2) = (3,2)
-  % x (2,3) .* y'(2,3) = (2,3)
-  % y'(2,3) .* x (2,3) = (2,3)
-  % y (3,2) .* x'(3,2) = (3,2)
+printf(" x (%d,%d)  * y (%d,%d)  = (%d,%d) | ", size(x ), size(y ), size(x  * y  )), ifelse(d, x  * y  , "")  % x (2,3)  * y (3,2) = (2,2)
+printf(" y (%d,%d)  * x (%d,%d)  = (%d,%d) | ", size(y ), size(x ), size(y  * x  )), ifelse(d, y  * x  , "")  % y (3,2)  * x (2,3) = (3,3)
+printf(" x'(%d,%d)  * y'(%d,%d)  = (%d,%d) | ", size(x'), size(y'), size(x' * y' )), ifelse(d, x' * y' , "")  % x'(3,2)  * y'(2,3) = (3,3)
+printf(" y'(%d,%d)  * x'(%d,%d)  = (%d,%d) | ", size(y'), size(x'), size(y' * x' )), ifelse(d, y' * x' , "")  % y'(2,3)  * x'(3,2) = (2,2)
+printf(" x'(%d,%d) .* y (%d,%d)  = (%d,%d) | ", size(x'), size(y ), size(x' .* y )), ifelse(d, x' .* y , "")  % x'(3,2) .* y (3,2) = (3,2)
+printf(" x (%d,%d) .* y'(%d,%d)  = (%d,%d) | ", size(x ), size(y'), size(x  .* y')), ifelse(d, x  .* y', "")  % x (2,3) .* y'(2,3) = (2,3)
+printf(" y'(%d,%d) .* x (%d,%d)  = (%d,%d) | ", size(y'), size(x ), size(y' .* x )), ifelse(d, y' .* x , "")  % y'(2,3) .* x (2,3) = (2,3)
+printf(" y (%d,%d) .* x'(%d,%d)  = (%d,%d) | ", size(y ), size(x'), size(y  .* x')), ifelse(d, y  .* x', "")  % y (3,2) .* x'(3,2) = (3,2)
 
 % UNDEFINED
 printf(" x (%d,%d)  * y'(%d,%d) : UNDEFINED\n", size(x ), size(y')); % x  * y'
 printf(" x'(%d,%d)  * y (%d,%d) : UNDEFINED\n", size(x'), size(y )); % x' * y
 printf(" y (%d,%d)  * x'(%d,%d) : UNDEFINED\n", size(y ), size(x')); % y  * x'
 printf(" y'(%d,%d)  * x (%d,%d) : UNDEFINED\n", size(y'), size(x )); % y' * x
-  % x (2,3)  * y'(2,3) : UNDEFINED
-  % x'(3,2)  * y (3,2) : UNDEFINED
-  % y (3,2)  * x'(3,2) : UNDEFINED
-  % y'(2,3)  * x (2,3) : UNDEFINED
 printf(" x (%d,%d) .* y (%d,%d) : UNDEFINED\n", size(x ), size(y )); % x  .* y
 printf(" y (%d,%d) .* x (%d,%d) : UNDEFINED\n", size(y ), size(x )); % y  .* x
 printf(" x'(%d,%d) .* y'(%d,%d) : UNDEFINED\n", size(x'), size(y')); % x' .* y'
 printf(" y'(%d,%d) .* x'(%d,%d) : UNDEFINED\n", size(y'), size(x')); % y' .* x'
+  % x (2,3)  * y'(2,3) : UNDEFINED
+  % x'(3,2)  * y (3,2) : UNDEFINED
+  % y (3,2)  * x'(3,2) : UNDEFINED
+  % y'(2,3)  * x (2,3) : UNDEFINED
   % x (2,3) .* y (3,2) : UNDEFINED
   % y (3,2) .* x (2,3) : UNDEFINED
   % x'(3,2) .* y'(2,3) : UNDEFINED
